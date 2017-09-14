ea9e771e508d0435acae2ca88c7c4a023389eaae
Janne Kujanpää <jikuja@iki.fi>
2017-09-14 21:15:01 +0300

    Updates
     * Added proper license
     * Removed extra space at beginning of sign descriptions
     * Removed spaces from sign ids
     * Updated data

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Liikennemerkkinumerot
 
-Tieliikenneasetuksen [luvun 3](http://finlex.fi/fi/laki/ajantasa/1982/19820182#L3) tietojen mukainen kuvaus merkin numerosta merkin nimeen. 
+Tieliikenneasetuksen [luvun 3](http://finlex.fi/fi/laki/ajantasa/1982/19820182#L3) tietojen mukainen 
+kuvaus merkin numerosta merkin nimeen. 
+
+JSON-tiedostoissa on data esitettynä kahdella eri tavalla avain/arvo-pareina arrayssä ja oliona, josta voi suoraan 
+merkin numerolla hakea merkin kuvauksen. Js-tiedostoissa data on JSONP-muodossa. Kovakoodattu
+callbackfunktio JSONP-datalle on `liikennemerkitCallback`.
+
+## Huomioita
+
+Cyclorama-datassa nopeusrajoituksien(merkit 361-364) tyyppitiedot sisältävät tiedon kilven nopeusrajoituksesta. 
+Esim `314_40` tarkoittaa nopeusrajoitusta 40 km/h. Nopeutta ei ole merkin tekstikentässä Turun eikä Helsingin datoissa. 
+Näitä variantteja ei ole lisätty tähän dataan, joten implementaation täytyy tarkistaa merkit 361-326 erikseen.
+
+Esimerkiksi merkit 411-415("pakollinen ajosuunta) ja 417ja 418 "liikententeenjakaja") vaativat lisädataa numeron 
+lisäksi merkin suunnasta. Cycloramadatassa tai muussa lähteessä voi olle selitteenä merkin suunta. Monen muun
+merkin kohdalla on samanlainen ongelma. Pelkällä numerolla ei pysty täydellisesti replikoimaan merkkiä.
+
+Merkit 424 ja 425 ovat molemmat nimellä "Pyörätie ja jalkakäytävä rinnakkain".
+
+Osaan merkkien numeroita on lisätty kirjaintunniste. Esim. merkit 521 a - 421 c. 
+Tässä datassa välissä olevat tyhjä merkki on poistettu datan saamiseksi samanmuotoiseksi kuin cyloramadatoissa
 
 ## Lisenssi
 Python-koodi datan päivittämiseksi ja data lisenssillä [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/)
--- update-data.py
+++ update-data.py
@@ -1,11 +1,30 @@
 #!/usb/bin/env python3
+import sys
+
+__author__ = "jikuja"
+__license__ = "CC0 1.0"
+__copyright__ = """<PROGRAM NAME> - <DESCRIPTION>
+    Written in 2016 by jikuja
+
+    To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to 
+    this software to the public domain worldwide. This software is distributed without any warranty.
+    
+    You should have received a copy of the CC0 Public Domain Dedication along with this software. 
+    If not, see <http://creativecommons.org/publicdomain/zero/1.0/>."
+"""
 
 import json
 import requests
 import re
 from lxml import html
 
+
+def add_data(num, txt):
+    data_array.append({"id": num, "value": txt})
+    data_map[num] = txt
+
 page = requests.get("http://finlex.fi/fi/laki/ajantasa/1982/19820182")
+# TODO: use local cache
 tree = html.fromstring(page.content)
 
 foos = tree.xpath('//p[@class="py"]/text()')
@@ -14,15 +33,24 @@ data_array = []
 data_map = {}
 
 for foo in foos:
+    print(foo, end="")
     if re.match("^\d+\.", foo) or re.match("^\d+ [a-zöäå]\.", foo):
-        splitted = foo.split(".")
+        print(" MATCH")
+        splitted = foo.split(". ")
         num = splitted[0]
         txt = splitted[1]
         data_array.append({"id": num, "value": txt})
         data_map[num] = txt
+    else:
+        print("")
 
-print(data_array)
-print(data_map)
+# fixes
+add_data("173", "Rautatien tasoristeyksen lähestymismerkki")
+add_data("174", "Rautatien tasoristeyksen lähestymismerkki")
+add_data("175", "Rautatien tasoristeyksen lähestymismerkki")
+
+if len(sys.argv) > 1 and sys.argv[1] == "dry":
+    sys.exit(0)
 
 with open("merkit_array.json", "w") as json_file:
     json_file.write(json.dumps(data_array))
@@ -35,4 +63,6 @@ with open("merkit_map.json", "w") as json_file:
 
 with open("merkit_map.js", "w") as json_file:
     json_file.write("liikennemerkitCallback(" +json.dumps(data_map) + ");")
+
+
 
